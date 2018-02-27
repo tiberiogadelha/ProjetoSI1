16257e2028401b02de94b909338ab8106865ede0
Tibério Gadelha <tiberio.gomes@ccc.ufcg.edu.br>
2018-02-27 18:30:41 -0300

    Atualizando front

--- Front3/js/config/config.js
+++ Front3/js/config/config.js
@@ -1,9 +1,16 @@
 app.config(
     function ($routeProvider) {
         $routeProvider
+            .when('/', {
+                templateUrl: 'index.html'
+            })
             .when('/produto', {
                 templateUrl: 'produto.html',
                 controller: 'produtoController'
+            })
+            .when('/lote', {
+                templateUrl: 'lote.html',
+                controller: 'loteController'
             })
             .otherwise({
                 redirectTo: '/'
